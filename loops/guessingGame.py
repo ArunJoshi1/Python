362c77c85978cdd51fc0ef2cd0caabb57db839c9
Arun Joshi <arunjoshi.cse@gmail.com>
2019-10-09 21:59:22 +0530

    changes in Guessing Game

--- loops/guessingGame.py
+++ loops/guessingGame.py
@@ -1,14 +1,16 @@
 import random
-ran=random.randrange(1,10,1);
-guess_count=0
-guess_limit=1
-while(guess_count!=guess_limit):
-    guess_count+=1
+
+ran = random.randrange(1, 10, 1);
+guess_count = 0
+guess_limit = 3
+print(f'Enter Number B/w 0 and 10')
+while guess_count != guess_limit:
+    guess_count += 1
     number = int(input("Enter a Number: "))
-    if(ran==number):
+    if ran == number:
         print(f'you won..Hurray {ran} equals to {number}')
         break
     else:
-        print(f'guess count remain {guess_limit-guess_count}')
-    if(guess_count==guess_limit):
+        print(f'guess count remain {guess_limit - guess_count}')
+    if guess_count == guess_limit:
         print(f'sorry you failed. random number is {ran}')
